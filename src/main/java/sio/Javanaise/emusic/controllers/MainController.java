fb32d3c4061d23a52f90c7f62f77bab4774cbf34
Twitline <geoffreylaunay02@gmail.com>
2022-10-19 11:55:56 +0200

    suppressionaceuil

--- src/main/java/sio/Javanaise/emusic/controllers/MainController.java
+++ src/main/java/sio/Javanaise/emusic/controllers/MainController.java
@@ -1,10 +1,5 @@
 package sio.Javanaise.emusic.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/emusic/")
 public class MainController {
 
 }
