dda212abd3e1c707a55092de5915d01ab8f6fa44
Twitline <geoffreylaunay02@gmail.com>
2022-10-19 11:48:18 +0200

    debutacceuilcontroller

--- src/main/java/sio/Javanaise/emusic/controllers/MainController.java
+++ src/main/java/sio/Javanaise/emusic/controllers/MainController.java
@@ -1,5 +1,10 @@
 package sio.Javanaise.emusic.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/emusic/")
 public class MainController {
 
 }
