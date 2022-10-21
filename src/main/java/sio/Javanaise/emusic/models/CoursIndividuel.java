2e37395819332c7cf4f4f3846b16a0ff032d4b0e
PaulG0 <paul.gouin@sts-sio-caen.info>
2022-10-21 09:52:24 +0200

    commit ma partie

--- src/main/java/sio/Javanaise/emusic/models/CoursIndividuel.java
+++ src/main/java/sio/Javanaise/emusic/models/CoursIndividuel.java
@@ -4,14 +4,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-public class Inscription {
+public class CoursIndividuel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
