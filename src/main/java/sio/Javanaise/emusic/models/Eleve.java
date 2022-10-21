e23a74401fd6c44d4780ad691df27686aae27b57
Twitline <geoffreylaunay02@gmail.com>
2022-10-21 09:52:58 +0200

    entity

--- src/main/java/sio/Javanaise/emusic/models/Eleve.java
+++ src/main/java/sio/Javanaise/emusic/models/Eleve.java
@@ -1,5 +1,28 @@
 package sio.Javanaise.emusic.models;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Eleve {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	private String nom;
+	private String prenom;
+	private LocalDateTime date_naiss;
 
 }
--- src/main/java/sio/Javanaise/emusic/models/Tranche.java
+++ src/main/java/sio/Javanaise/emusic/models/Tranche.java
@@ -1,5 +1,22 @@
 package sio.Javanaise.emusic.models;
 
-public class Tranche {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Tranche {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	private int quotient_min;
 }
