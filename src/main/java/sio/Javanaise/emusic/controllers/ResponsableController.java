18a2d4d65933540a7f843367f518ac8cb831e169
Louka-Fauvel <73880235+Louka-Fauvel@users.noreply.github.com>
2022-11-05 15:53:07 +0100

    Fix : GI-Gest-23

--- src/main/java/sio/Javanaise/emusic/controllers/ResponsableController.java
+++ src/main/java/sio/Javanaise/emusic/controllers/ResponsableController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -61,6 +62,14 @@ public class ResponsableController {
     @GetMapping("/new")
     public String newAction() {
     	return "/responsables/form";
+    }
+    
+    @PostMapping("/new")
+    public RedirectView newAction(@ModelAttribute Responsable responsable) {
+    	
+    	responsablerepo.save(responsable);
+    	return new RedirectView("/responsables/index");
+    	
     }
     
     @GetMapping("/delete/{role}/{id}")
