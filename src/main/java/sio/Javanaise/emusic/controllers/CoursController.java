03adabcb966e5c219c7c7d7f666c3fdd9b763646
Louka-Fauvel <73880235+Louka-Fauvel@users.noreply.github.com>
2022-11-06 17:34:04 +0100

    First commit

--- src/main/java/sio/Javanaise/emusic/controllers/CoursController.java
+++ src/main/java/sio/Javanaise/emusic/controllers/CoursController.java
@@ -2,11 +2,17 @@ package sio.Javanaise.emusic.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import io.github.jeemv.springboot.vuejs.VueJS;
+import sio.Javanaise.emusic.models.Cour;
+import sio.Javanaise.emusic.repositories.ICoursRepository;
 
 @Controller
 @RequestMapping({ "/cours", "/cours/" })
@@ -19,9 +25,45 @@ public class CoursController {
     public VueJS getVue() {
         return this.vue;
     }
+    
+    @Autowired
+    private ICoursRepository coursRepository;
 
     @GetMapping("")
     public String indexCoursAction() {
         return "/cours/index";
     }
+    
+    @GetMapping("/new")
+    public String coursNewAction(ModelMap model) {
+    	
+    	model.put("cour", new Cour());
+    	return "/cours/form";
+    	
+    }
+    
+    @PostMapping("/new")
+    public RedirectView CoursNewAction(@ModelAttribute Cour cour) {
+    	
+    	coursRepository.save(cour);
+    	return new RedirectView("/cours");
+    	
+    }
+    
+    @GetMapping("/delete/{id}")
+    public RedirectView coursDeleteAction(@PathVariable int id) {
+    	
+    	coursRepository.deleteById(id);
+    	return new RedirectView("/cours");
+    	
+    }
+    
+    @GetMapping("/edit/{id}")
+    public String coursEditAction(ModelMap model, @PathVariable int id) {
+    	
+    	coursRepository.findById(id).ifPresent(cour -> model.put("cour", cour));
+    	return "/cours/form";
+    	
+    }
+    
 }
