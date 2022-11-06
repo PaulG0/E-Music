1df3e5d548e1fdb651688522515ee67f69590335
Twitline <geoffreylaunay02@gmail.com>
2022-11-06 21:19:49 +0100

    debutacceuil

--- src/main/java/sio/Javanaise/emusic/configs/WebSecurityConfig.java
+++ src/main/java/sio/Javanaise/emusic/configs/WebSecurityConfig.java
@@ -28,7 +28,8 @@ public class WebSecurityConfig {
 
 	@Bean // (2)
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/css/**", "/index", "/new/", "/new", "/h2-console/**", "/webjars/**")
+		http.authorizeRequests()
+				.antMatchers("/", "/images/**", "/css/**", "/index", "/new/", "/new", "/h2-console/**", "/webjars/**")
 				.permitAll() // (3)
 				.anyRequest().authenticated() // (4)
 				.and().formLogin() // (5)
