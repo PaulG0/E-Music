18f6607a43c78d0c6bc5da39f007b8d6c15b4785
PaulG0 <paul.gouin@sts-sio-caen.info>
2022-11-09 20:10:28 +0100

    Update devController.java

--- src/main/java/sio/Javanaise/emusic/controllers/devController.java
+++ src/main/java/sio/Javanaise/emusic/controllers/devController.java
@@ -9,12 +9,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.javafaker.Faker;
 
+import sio.Javanaise.emusic.enumeration.TypeCourEnum;
+import sio.Javanaise.emusic.models.Cour;
 import sio.Javanaise.emusic.models.Eleve;
 import sio.Javanaise.emusic.models.Instrument;
 import sio.Javanaise.emusic.models.Prof;
+import sio.Javanaise.emusic.models.TypeCour;
+import sio.Javanaise.emusic.repositories.ICoursRepository;
 import sio.Javanaise.emusic.repositories.IEleveRepository;
 import sio.Javanaise.emusic.repositories.IInstrumentRepository;
 import sio.Javanaise.emusic.repositories.IProfRepository;
+import sio.Javanaise.emusic.repositories.ITypeCoursRepository;
 
 @Controller
 @RequestMapping("/data")
@@ -29,8 +34,22 @@ public class devController {
 	@Autowired
 	private IInstrumentRepository instruRepo;
 
+	@Autowired
+	private ITypeCoursRepository typeCourRepo;
+
+	@Autowired
+	private ICoursRepository courRepo;
+
 	@GetMapping("/{nb}")
 	private @ResponseBody String ajoutData(@PathVariable int nb) {
+
+		// type de Cour
+		TypeCour typeCour = new TypeCour(TypeCourEnum.Individuel);
+		typeCour.setId(2);
+		typeCourRepo.save(typeCour);
+		typeCour = new TypeCour(TypeCourEnum.Collectif);
+		typeCour.setId(1);
+		typeCourRepo.save(typeCour);
 
 		for (int o = 0; o < nb; o++) {
 
@@ -42,6 +61,7 @@ public class devController {
 			profRepo.save(prof);
 
 			// eleve
+
 			fake = new Faker();
 			Eleve eleve = new Eleve();
 
@@ -55,6 +75,17 @@ public class devController {
 			instru.setIntitule(fake.music().instrument());
 			instruRepo.save(instru);
 
+			// cour
+			fake = new Faker();
+			Cour cour = new Cour();
+			cour.setAgeMAx(15);
+			cour.setAgeMin(9);
+			cour.setLibelle(fake.music().genre());
+			cour.setNbPlace(5);
+			cour.setProf(prof);
+			cour.setTypeCour(typeCour);
+
+			courRepo.save(cour);
 		}
 		return "init ok";
 	}
