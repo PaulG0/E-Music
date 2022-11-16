336fc51f7f948a4a21c19bb1ca7b738113460393
Louka-Fauvel <73880235+Louka-Fauvel@users.noreply.github.com>
2022-11-16 09:01:36 +0100

    Fix : planning

--- src/main/java/sio/Javanaise/emusic/controllers/PlanningController.java
+++ src/main/java/sio/Javanaise/emusic/controllers/PlanningController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import io.github.jeemv.springboot.vuejs.VueJS;
 import sio.Javanaise.emusic.models.Planning;
@@ -50,6 +52,15 @@ public class PlanningController {
 		ArrayList<Planning> planning = planService.planningProf(id);
 		model.put("planning", planning);
 		return "/planning/prof";
+	}
+	
+	@PostMapping("/prof/{id}")
+	private String planingProfAction(@PathVariable int id, ModelMap model, @RequestParam String date) {
+		
+		ArrayList<Planning> planning = planService.planningProf(id);
+		model.put("planning", planning);
+		return "/planning/prof";
+		
 	}
 
 }
