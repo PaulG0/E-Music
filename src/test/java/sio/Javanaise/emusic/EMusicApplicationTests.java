fbe14c66988aeec2815f4b8d330ab1e33f562125
PaulG0 <paul.gouin@sts-sio-caen.info>
2022-11-28 10:20:43 +0100

    Update EMusicApplicationTests.java

--- src/test/java/sio/Javanaise/emusic/EMusicApplicationTests.java
+++ src/test/java/sio/Javanaise/emusic/EMusicApplicationTests.java
@@ -1,13 +1,10 @@
 package sio.Javanaise.emusic;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EMusicApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
