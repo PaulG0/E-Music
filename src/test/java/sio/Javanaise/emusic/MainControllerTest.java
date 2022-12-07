93e3cbf0126b40c8c75965e9f54ff6e5a27029fb
Louka-Fauvel <73880235+Louka-Fauvel@users.noreply.github.com>
2022-12-07 12:57:03 +0100

    Feat : les test ne marchent pas

--- src/test/java/sio/Javanaise/emusic/MainControllerTest.java
+++ src/test/java/sio/Javanaise/emusic/MainControllerTest.java
@@ -1,12 +1,18 @@
 package sio.Javanaise.emusic;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import sio.javanaise.emusic.configs.WebSecurityConfig;
 import sio.javanaise.emusic.models.User;
+import sio.javanaise.emusic.services.TokenGenerator;
+import sio.javanaise.emusic.services.UserService;
 
 @WebMvcTest(MainControllerTest.class)
 @ContextConfiguration(classes = {WebSecurityConfig.class, EMusicApplicationTests.class})
@@ -15,10 +21,23 @@ public class MainControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private UserDetailsService uService;
+
+    @MockBean
+    private TokenGenerator tokgen;
+
     @Test
     void newActionTest() throws Exception {
 
-        User user = new User();
+        User user = ((UserService) uService).createUser("Test-Test", "Azerty1234");
+        String token = tokgen.generateToken();
+        user.setToken(token);
+        user.setAuthorities("PARENT");
+        this.mockMvc.perform(post("/e-music/new").contentType(MediaType.APPLICATION_JSON)
+                .content(
+                        "{\"token\":\"" + token + "\",\"user\":\"" + user.getNickname() + "\"}"))
+                );
 
     }
 
