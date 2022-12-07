70ced34b4b8e3ea41b0b88f408d2c63192a63980
Louka-Fauvel <73880235+Louka-Fauvel@users.noreply.github.com>
2022-12-07 11:01:15 +0100

    feat : début test fonction

---
+++ src/test/java/sio/Javanaise/emusic/MainControllerTest.java
@@ -0,0 +1,17 @@
+package sio.Javanaise.emusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import sio.javanaise.emusic.configs.WebSecurityConfig;
+
+@WebMvcTest(MainControllerTest.class)
+@ContextConfiguration(classes = {WebSecurityConfig.class, EMusicApplicationTests.class})
+public class MainControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+}
