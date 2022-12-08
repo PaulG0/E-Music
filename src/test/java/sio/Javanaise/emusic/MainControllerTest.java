f19090cf3e5e5aa5a1987da0d39ad2d4104e8ed2
Louka-Fauvel <73880235+Louka-Fauvel@users.noreply.github.com>
2022-12-08 21:35:35 +0100

    Feat : test

--- src/test/java/sio/Javanaise/emusic/MainControllerTest.java
+++ src/test/java/sio/Javanaise/emusic/MainControllerTest.java
@@ -1,18 +1,14 @@
 package sio.Javanaise.emusic;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import sio.javanaise.emusic.configs.WebSecurityConfig;
-import sio.javanaise.emusic.models.User;
-import sio.javanaise.emusic.services.TokenGenerator;
-import sio.javanaise.emusic.services.UserService;
 
 @WebMvcTest(MainControllerTest.class)
 @ContextConfiguration(classes = {WebSecurityConfig.class, EMusicApplicationTests.class})
@@ -21,22 +17,39 @@ public class MainControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private UserDetailsService uService;
-
-    @MockBean
-    private TokenGenerator tokgen;
-
     @Test
     void newActionTest() throws Exception {
 
-        User user = ((UserService) uService).createUser("Test-Test", "Azerty1234");
-        String token = tokgen.generateToken();
-        user.setToken(token);
-        user.setAuthorities("PARENT");
-        this.mockMvc.perform(post("/e-music/new").contentType(MediaType.APPLICATION_JSON)
-                .content(
-                        "{\"token\":\"" + token + "\",\"user\":\"" + user.getNickname() + "\"}"))
+        String login = "Test-Testnew";
+        String civilite = "Mr";
+        String nom = "Testnew";
+        String prenom = "Test";
+        String adresse = "12 rue Fermat";
+        String ville = "Caen";
+        Integer quotient_familial = 0;
+        String code_postal = "14000";
+        String email = "test@test.com";
+        String password = "Azerty1234";
+        String tel1 = "0674018434";
+        String tel2 = "";
+        String tel3 = "";
+        this.mockMvc
+                .perform(post("/e-music/new").contentType(MediaType.APPLICATION_JSON)
+                    .content("{\"login\":\"" + login + "\"," +
+                            "\"civilite\":\"" + civilite + "\"," +
+                            "\"nom\":\"" + nom + "\"," +
+                            "\"prenom\":\"" + prenom + "\"," +
+                            "\"adresse\":\"" + adresse + "\"," +
+                            "\"ville\":\"" + ville + "\"," +
+                            "\"quotient_familial\":\"" + quotient_familial + "\"," +
+                            "\"code_postal\":\"" + code_postal + "\"," +
+                            "\"email\":\"" + email + "\"," +
+                            "\"password\":\"" + password + "\"," +
+                            "\"tel1\":\"" + tel1 + "\"," +
+                            "\"tel2\":\"" + tel2 + "\"," +
+                            "\"tel3\":\"" + tel3 + "\"" +
+                            "}"
+                    ).with(csrf())
                 );
 
     }
